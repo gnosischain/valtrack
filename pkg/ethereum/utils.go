e16a4e9f601061bb54c5cfe45d9f142cfc149511
riccardo <106812074+riccardo-gnosis@users.noreply.github.com>
2024-09-06 11:20:26 +0200

    add: machine and region gathering

--- pkg/ethereum/utils.go
+++ pkg/ethereum/utils.go
@@ -12,14 +12,21 @@ func getCrawlerLocation() string {
 	if region := os.Getenv("FLY_REGION"); region != "" {
 		return region
 	}
-	return "FLY_REGION"
+	if region := os.Getenv("AWS_REGION"); region != "" {
+		return region
+	}
+
+	return "UNKOWN_REGION"
 }
 
 func getCrawlerMachineID() string {
 	if id := os.Getenv("FLY_MACHINE_ID"); id != "" {
 		return id
 	}
-	return "FLY_MACHINE_ID"
+	if id := os.Getenv("HOSTNAME"); id != "" {
+		return id
+	}
+	return "UNKOWN_MACHINE_ID"
 }
 
 // MaddrFrom takes in an ip address string and port to produce a go multiaddr format.
